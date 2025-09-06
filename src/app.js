e9aba0cfaabd0f654dd7739e10f3ec628d9b05f7
chirag-sinha <chiragsinha73@gmail.com>
2025-09-06 09:09:05 +0530

    playing with HTTP methods

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const app = express();
 //app.listen(3000);
 
 //now to handle the reuqests , we use app.use 
+
+// app.use("/" ,(req,res) => {
+//     res.send("hello from the server side dashboard ....");
+// })//this will be over ridden in all the cases .
+
 app.get("/" ,(req,res) => {
     res.send("hello from the server side dashboard ....");
 })
@@ -14,6 +19,21 @@ app.get("/test", (req,res) => {
 
 app.get("/home", (req,res) => {
     res.send("you are at home server ....");
+})
+
+app.get("/user" , (req,res) => {
+    res.send({firstname : "Chirag " , lastname: "Sinha"});
+})
+
+//postcall
+app.post("/user" , (req,res) => {
+    console.log("data saved");
+    res.send("hello the user data has been saved to the DB!!!");
+})
+
+//deletecall
+app.delete("/user" , (req,res) => {
+    res.send("deleted successfully !!");
 })
 
 app.listen(3000 , () => {
