de8c66fad506c286e9e9bbb2fd6c3bd58c635fa8
chirag-sinha <chiragsinha73@gmail.com>
2025-09-11 16:48:34 +0530

    schema validations on fields

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,47 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName : {
-        type:String 
+        type:String, 
+        required: true,
+        minLength: 3,
+        maxLength: 30,
    },
    lastName : {
-    type:String
+        type:String,
+
    },
    emailID : {
-    type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
    },
    password : {
-    type:String
+    type:String,
+    required:true,
    },
    age : {
     type:Number
    },
    gender : {
-    type: String
+    type: String,
+    validate (value){
+        if(!["male" , "female" , "others"].includes(value)){
+            throw new Error("Gender not valid!!!");
+        }
+    }
    },
+   about :{
+    type:String ,
+    default:"this is the default description of user",
+   },
+   skills : {
+    type:[String],
+   } 
+},
+{
+    timestamps:true,
 });
 
 module.exports =  mongoose.model("User" , userSchema);
